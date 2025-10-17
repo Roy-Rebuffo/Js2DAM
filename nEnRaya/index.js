efd799c7f6ebfb7ee96faa6bf502245180997faa
Roysao <roysao98@gmail.com>
2025-10-17 11:49:34 +0200

    nEnRaya

---
+++ nEnRaya/index.js
@@ -0,0 +1,22 @@
+//2*n + 2
+
+function inicio(){
+    let $select = document.getElementById("sOpt").value;
+    console.log($select);
+
+    let cadena = "";
+
+    cadena += "<table border = '1' margin = 'center'>";
+    cadena += "<thead>";
+    cadena += "<tr>";
+    
+
+    cadena += "</tr>"
+    cadena += "</thead>";
+    cadena += "</table>";
+}
+
+
+
+
+window.onload = () => {inicio();};
